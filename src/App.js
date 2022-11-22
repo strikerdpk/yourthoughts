8f8d492513eb3c91dc47b273287b2e77e6468ec0
Deepak Kushwaha <deepakkushwaha@Deepaks-Air.bbrouter>
2022-11-22 13:26:53 +0530

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+// import { Item } from "semantic-ui-react";
+import "./App.css";
+import Product from "./components/Product";
+import Seed from "./components/Seed";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This is app component</h1>
+      <Seed />
     </div>
   );
 }
